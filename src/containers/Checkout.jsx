9d5739da835ae0a09a8602f05806bdbf84c2d8d1
Gonn852 <gonn852@gmail.com>
2021-11-02 22:19:07 -0300

    add styles

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Checkout = () => {
     return(
-        <h1> Checkout</h1>
+        <h1>Checkout</h1>
     )
 }
 
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -7,19 +7,24 @@ import Information from '../containers/Information';
 import Payment from '../containers/Payment';
 import Success from '../containers/Success';
 import NotFound from '../containers/NotFound';
+import Layout from '../components/Layout'
+
+import '../styles/components/Layout.css'
 
 
 const App = () => {
     return(
         <BrowserRouter>
-            <Switch>
-                <Route exact path="/" component={Home}></Route>
-                <Route exact path="/checkout" component={Checkout}></Route>
-                <Route exact path="/checkout/information" component={Information}></Route>
-                <Route exact path="/checkout/payment" component={Payment}></Route>
-                <Route exact path="/checkout/success" component={Success}></Route>
-                <Route component={NotFound}></Route>
-            </Switch>
+            <Layout>
+                <Switch>
+                    <Route exact path="/" component={Home}></Route>
+                    <Route exact path="/checkout" component={Checkout}></Route>
+                    <Route exact path="/checkout/information" component={Information}></Route>
+                    <Route exact path="/checkout/payment" component={Payment}></Route>
+                    <Route exact path="/checkout/success" component={Success}></Route>
+                    <Route component={NotFound}></Route>
+                </Switch>
+            </Layout>
         </BrowserRouter>
     )
 }
