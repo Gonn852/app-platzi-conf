2364675004a6a02158b5b1bcde53e173d03c17f7
goenix <gonn852@gmail.com>
2021-11-04 15:33:15 -0300

    add icons and react-router-dom (Link)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/components/Header.css'
 
 const Header = () => {
     return (
         <div className="Header">
-            <h1 className="Header-title">PlatziConf Merch</h1>
+            <Link to="/">
+                <h1 className="Header-title">PlatziConf Merch</h1>
+            </Link>
             <div className="Header-checkout">
-                Checkout
+                <Link to="/checkout"><i className="fas fa-shopping-basket"></i></Link>
             </div>
         </div>    
     )
