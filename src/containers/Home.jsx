3eb5603112e4f454e69a6d1b3a18e92115a17eff
goenix <gonn852@gmail.com>
2021-11-18 10:00:19 -0300

    Revert "add react-helmet"
    This reverts commit ae5ead8b5c228705d368046c24530485e98aeda5.

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,16 +1,10 @@
 import React from 'react'
 import initialState from '../initialState';
 import Products from '../components/Products';
-import { Helmet } from 'react-helmet';
 
 const Home = () => {
     return(
-        <>
-            <Helmet>
-               <meta property="og:image" content="https://vabadus.es/images/cache/imagen_nodo/images/articulos/5c9deedea0c7e844300455.png" />
-            </Helmet>
-            <Products products={initialState.products}></Products>  
-        </>
+        <Products products={initialState.products}></Products>
     )
 }
 
