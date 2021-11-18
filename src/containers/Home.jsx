ae5ead8b5c228705d368046c24530485e98aeda5
goenix <gonn852@gmail.com>
2021-11-18 09:34:52 -0300

    add react-helmet

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import initialState from '../initialState';
 import Products from '../components/Products';
+import { Helmet } from 'react-helmet';
 
 const Home = () => {
     return(
-        <Products products={initialState.products}></Products>
+        <>
+            <Helmet>
+               <meta property="og:image" content="https://vabadus.es/images/cache/imagen_nodo/images/articulos/5c9deedea0c7e844300455.png" />
+            </Helmet>
+            <Products products={initialState.products}></Products>  
+        </>
     )
 }
 
